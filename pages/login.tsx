715b65bb9dd7f6ac805e153c48e9c8d42565ba6a
Asif Ramzan <asiframzan2021@gmail.com>
2022-08-17 15:42:21 +0500

    login page created

---
+++ pages/login.tsx
@@ -0,0 +1,55 @@
+import styles from '../styles/Login.module.css'
+
+const login = () => {
+  return (
+    <>
+  <div className={styles.main_div}>
+        <div className={styles.inner_div}>
+       <form className={styles.form}>
+        <h1 className='text-center'>Sign In</h1>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+        <div className="d-grid">
+          <button type="submit">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </form>
+
+        </div>
+
+        </div>
+        </>
+  )
+}
+
+export default login
