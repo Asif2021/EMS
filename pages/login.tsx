768943101b9a21c3a71343c79146cb3cabb66b73
Asif Ramzan <asiframzan2021@gmail.com>
2022-08-18 14:58:44 +0500

    yup package installed and made validation on form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,31 @@
 import styles from '../styles/Login.module.css'
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+
 
 const login = () => {
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+  useFormik({
+    initialValues:{
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email().required("Please enter your email"),
+      password: Yup.string().min(6).required("Please enter your password"),
+   }),
+    onSubmit: (values, action) => {
+      alert(`your email is ${values.email} and your password is ${values.password}`);
+      action.resetForm();
+    },
+  });
+  
   return (
     <>
   <div className={styles.main_div}>
         <div className={styles.inner_div}>
-       <form className={styles.form}>
+       <form className={styles.form} onSubmit={handleSubmit}>
         <h1 className='text-center'>Sign In</h1>
         <div className="mb-3">
           <label>Email address</label>
@@ -13,7 +33,16 @@ const login = () => {
             type="email"
             className="form-control"
             placeholder="Enter email"
+            autoComplete="off"
+            name="email"
+            id="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
+          {errors.email && touched.email ? (
+                      <span className={styles.form_error}>{errors.email}</span>
+                    ) : null}
         </div>
         <div className="mb-3">
           <label>Password</label>
@@ -21,7 +50,16 @@ const login = () => {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            autoComplete="off"
+            name="password"
+            id="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
+           {errors.password && touched.password ? (
+                      <span className={styles.form_error}>{errors.password}</span>
+                    ) : null}
         </div>
         <div className="mb-3">
           <div className="custom-control custom-checkbox">
