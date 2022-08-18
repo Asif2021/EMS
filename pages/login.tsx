e10a93d32b3aa136a195ba529d9c85e4681b5c69
Asif Ramzan <asiframzan2021@gmail.com>
2022-08-18 15:40:07 +0500

    added some styles and include comments in login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,24 +1,35 @@
+// import styles 
 import styles from '../styles/Login.module.css'
+// import useFormik hook from formik
 import { useFormik } from "formik";
+// import everything as yup from yup
 import * as Yup from "yup";
+// imprt useRouter from next/router package
 import { useRouter } from 'next/router'
 
 
 
 const login = () => {
+  // made a variable named router from useRouter hook
   const router = useRouter()
+  // destructuring from useFormik hook
   const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
   useFormik({
+    // initialValues object containing initial values of the form
     initialValues:{
       email: "",
       password: "",
     },
+    // putting validation on the form
     validationSchema: Yup.object({
       email: Yup.string().email().required("Please enter your email"),
       password: Yup.string().min(6).required("Please enter your password"),
    }),
+  //  declaring onSubmit function
     onSubmit: (values, action) => {
-       router.push('/dashboard')
+// routing to the Dashboards
+      router.push('/dashboard')
+      // resetting the form in empty state
       action.resetForm();
     },
   });
@@ -27,8 +38,11 @@ const login = () => {
     <>
   <div className={styles.main_div}>
         <div className={styles.inner_div}>
+          {/* form start */}
        <form className={styles.form} onSubmit={handleSubmit}>
+        {/* Sign In heading  */}
         <h1 className='text-center'>Sign In</h1>
+        {/* Email address input field */}
         <div className="mb-3">
           <label>Email address</label>
           <input
@@ -46,6 +60,7 @@ const login = () => {
                       <span className={styles.form_error}>{errors.email}</span>
                     ) : null}
         </div>
+        {/* Password input field */}
         <div className="mb-3">
           <label>Password</label>
           <input
@@ -63,6 +78,7 @@ const login = () => {
                       <span className={styles.form_error}>{errors.password}</span>
                     ) : null}
         </div>
+
         <div className="mb-3">
           <div className="custom-control custom-checkbox">
             <input
@@ -75,11 +91,13 @@ const login = () => {
             </label>
           </div>
         </div>
+        {/* Submit Button */}
         <div className="d-grid">
           <button type="submit">
             Submit
           </button>
         </div>
+        {/* forgot password option */}
         <p className="forgot-password text-right">
           Forgot <a href="#">password?</a>
         </p>
