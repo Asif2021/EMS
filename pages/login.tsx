7bdee31023d40444ffb9d7d3bf0ee62eb724b4ef
Asif Ramzan <asiframzan2021@gmail.com>
2022-08-21 01:19:49 +0500

    new responsive login page replaced withold one

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,18 +1,14 @@
-// import styles 
-import styles from '../styles/Login.module.css'
-// import useFormik hook from formik
+// importing useFormik hook 
 import { useFormik } from "formik";
-// import everything as yup from yup
+// importing everything as yup
 import * as Yup from "yup";
-// imprt useRouter from next/router package
+// importing useRouter from next/router package
 import { useRouter } from 'next/router'
-
-
+// importing styles from styles
+import styles from '../styles/Login.module.css'
 
 const login = () => {
-  // made a variable named router from useRouter hook
   const router = useRouter()
-  // destructuring from useFormik hook
   const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
   useFormik({
     // initialValues object containing initial values of the form
@@ -33,80 +29,74 @@ const login = () => {
       action.resetForm();
     },
   });
-  
   return (
     <>
-  <div className={styles.main_div}>
-        <div className={styles.inner_div}>
-          {/* form start */}
-       <form className={styles.form} onSubmit={handleSubmit}>
-        {/* Sign In heading  */}
-        <h1 className='text-center'>Sign In</h1>
-        {/* Email address input field */}
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            autoComplete="off"
+  <div className={styles.body_div}>
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div className="card border-0 shadow rounded-3 my-5">
+          <div className="card-body p-4 p-sm-5">
+            <h5 className="card-title text-center mb-4 fw-light fs-5">Sign In</h5>
+            {/* form starting here */}
+            <form onSubmit={handleSubmit}>
+              {/* first input field to get email */}
+              <div className="form-floating mb-3">
+                <input type="email" className="form-control" id="email" placeholder="name@example.com" autoComplete="off"
             name="email"
-            id="email"
             value={values.email}
             onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {errors.email && touched.email ? (
-                      <span className={styles.form_error}>{errors.email}</span>
-                    ) : null}
-        </div>
-        {/* Password input field */}
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            autoComplete="off"
+            onBlur={handleBlur}/>
+                <label>Email address</label>
+              {errors.email && touched.email ? (
+                <span className={styles.form_error}>{errors.email}</span>
+                ) : null}
+                </div>
+                {/* second input field to get password */}
+              <div className="form-floating mb-3">
+                <input type="password" className="form-control" placeholder="Password" autoComplete="off"
             name="password"
             id="password"
             value={values.password}
             onChange={handleChange}
-            onBlur={handleBlur}
-          />
-           {errors.password && touched.password ? (
-                      <span className={styles.form_error}>{errors.password}</span>
-                    ) : null}
-        </div>
-
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
+            onBlur={handleBlur}/>
+                <label>Password</label>
+              {errors.password && touched.password ? (
+                <span className={styles.form_error}>{errors.password}</span>
+                ) : null}
+                </div>
+{/* remember password option */}
+              <div className="form-check mb-3">
+                <input className="form-check-input" type="checkbox" value="" id="rememberPasswordCheck"/>
+                <label className="form-check-label">
+                  Remember password
+                </label>
+              </div>
+              {/* submit button */}
+              <div className="d-grid">
+                <button className="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Sign
+                  in</button>
+              </div>
+              <hr className="my-4"/>
+              <div className="d-grid mb-2">
+                <button className="btn btn-google btn-login text-uppercase fw-bold" type="submit">
+                  <i className="fab fa-google me-2"></i> Sign in with Google
+                </button>
+              </div>
+              <div className="d-grid">
+                <button className="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
+                  <i className="fab fa-facebook-f me-2"></i> Sign in with Facebook
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-        {/* Submit Button */}
-        <div className="d-grid">
-          <button type="submit">
-            Submit
-          </button>
-        </div>
-        {/* forgot password option */}
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </form>
+      </div>
+    </div>
+  </div>
+  </div>
 
-        </div>
-
-        </div>
-        </>
+    </>
   )
 }
 
