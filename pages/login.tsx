e19aa69562ab25d9a08a151e53311f21870ba810
Asif Ramzan <asiframzan2021@gmail.com>
2022-08-21 21:33:32 +0500

    new login page template added

--- pages/login.tsx
+++ pages/login.tsx
@@ -31,70 +31,57 @@ const login = () => {
   });
   return (
     <>
-  <div className={styles.body_div}>
-  <div className="container">
-    <div className="row">
-      <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div className="card border-0 shadow rounded-3 my-5">
-          <div className="card-body p-4 p-sm-5">
-            <h5 className="card-title text-center mb-4 fw-light fs-5">Sign In</h5>
-            {/* form starting here */}
-            <form onSubmit={handleSubmit}>
-              {/* first input field to get email */}
-              <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="email" placeholder="name@example.com" autoComplete="off"
-            name="email"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}/>
-                <label>Email address</label>
-              {errors.email && touched.email ? (
-                <span className={styles.form_error}>{errors.email}</span>
-                ) : null}
-                </div>
-                {/* second input field to get password */}
-              <div className="form-floating mb-3">
-                <input type="password" className="form-control" placeholder="Password" autoComplete="off"
-            name="password"
-            id="password"
-            value={values.password}
-            onChange={handleChange}
-            onBlur={handleBlur}/>
-                <label>Password</label>
-              {errors.password && touched.password ? (
-                <span className={styles.form_error}>{errors.password}</span>
-                ) : null}
-                </div>
-{/* remember password option */}
-              <div className="form-check mb-3">
-                <input className="form-check-input" type="checkbox" value="" id="rememberPasswordCheck"/>
-                <label className="form-check-label">
-                  Remember password
-                </label>
-              </div>
-              {/* submit button */}
-              <div className="d-grid">
-                <button className="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Sign
-                  in</button>
-              </div>
-              <hr className="my-4"/>
-              <div className="d-grid mb-2">
-                <button className="btn btn-google btn-login text-uppercase fw-bold" type="submit">
-                  <i className="fab fa-google me-2"></i> Sign in with Google
-                </button>
-              </div>
-              <div className="d-grid">
-                <button className="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
-                  <i className="fab fa-facebook-f me-2"></i> Sign in with Facebook
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
+	<body>
+	<section className="ftco-section">
+		<div className="container">
+			<div className="row justify-content-center">
+				<div className="col-md-6 text-center mb-5">
+					<h2 className="heading-section">Login #09</h2>
+				</div>
+			</div>
+			<div className="row justify-content-center">
+				<div className="col-md-6 col-lg-4">
+					<div className="login-wrap py-5">
+		      	<div className="img d-flex align-items-center justify-content-center"> <span className={styles.image}> </span></div>
+		      	<h3 className="text-center mb-0">Welcome</h3>
+		      	<p className="text-center">Sign in by entering the information below</p>
+						<form action="#" className="login-form">
+		      		<div className="form-group">
+		      			<div className="icon d-flex align-items-center justify-content-center"><span className="fa fa-user"></span></div>
+		      			<input type="text" className="form-control" placeholder="Username" required/>
+		      		</div>
+	            <div className="form-group">
+	            	<div className="icon d-flex align-items-center justify-content-center"><span className="fa fa-lock"></span></div>
+	              <input type="password" className="form-control" placeholder="Password" required/>
+	            </div>
+	            <div className="form-group d-md-flex">
+								<div className="w-100 text-md-right">
+									<a href="#">Forgot Password</a>
+								</div>
+	            </div>
+	            <div className="form-group">
+	            	<button type="submit" className="btn form-control btn-primary rounded submit px-3">Get Started</button>
+	            </div>
+	          </form>
+	          <div className="w-100 text-center mt-4 text">
+	          	<p className="mb-0">Don't have an account?</p>
+		          <a href="#">Sign Up</a>
+	          </div>
+	        </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+
+
+
 
     </>
   )
