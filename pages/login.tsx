233c168859dade280006be2615b588759a22ba1f
Asif Ramzan <asiframzan2021@gmail.com>
2022-08-22 00:57:42 +0500

    login page added

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,38 +1,10 @@
-// importing useFormik hook 
-import { useFormik } from "formik";
-// importing everything as yup
-import * as Yup from "yup";
-// importing useRouter from next/router package
-import { useRouter } from 'next/router'
-// importing styles from styles
-import styles from '../styles/Login.module.css'
+import styles from '../styles/login.module.css'
 
-const login = () => {
-  const router = useRouter()
-  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
-  useFormik({
-    // initialValues object containing initial values of the form
-    initialValues:{
-      email: "",
-      password: "",
-    },
-    // putting validation on the form
-    validationSchema: Yup.object({
-      email: Yup.string().email().required("Please enter your email"),
-      password: Yup.string().min(6).required("Please enter your password"),
-   }),
-  //  declaring onSubmit function
-    onSubmit: (values, action) => {
-// routing to the Dashboards
-      router.push('/dashboard')
-      // resetting the form in empty state
-      action.resetForm();
-    },
-  });
-  return (
-    <>
-	<body>
-	<section className="ftco-section">
+export default function login () {
+
+return (
+<>
+<section className="ftco-section">
 		<div className="container">
 			<div className="row justify-content-center">
 				<div className="col-md-6 text-center mb-5">
@@ -42,7 +14,7 @@ const login = () => {
 			<div className="row justify-content-center">
 				<div className="col-md-6 col-lg-4">
 					<div className="login-wrap py-5">
-		      	<div className="img d-flex align-items-center justify-content-center"> <span className={styles.image}> </span></div>
+		      	<div className="img d-flex align-items-center justify-content-center bg_image" ></div>
 		      	<h3 className="text-center mb-0">Welcome</h3>
 		      	<p className="text-center">Sign in by entering the information below</p>
 						<form action="#" className="login-form">
@@ -72,19 +44,7 @@ const login = () => {
 			</div>
 		</div>
 	</section>
-
-	<script src="js/jquery.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>
-
-	</body>
-
-
-
-
-    </>
+ </>
   )
 }
 
-export default login
